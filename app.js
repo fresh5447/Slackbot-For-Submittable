dca2d37186a09e05743db36f274d8e75bbd838e5
Doug Walter <doug@kosmojo.com>
2015-08-22 14:46:03 +0200

    dicebot

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
+var dicebot = require('./dicebot');
+
+
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -8,7 +11,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){res.status(200).json("it's working")});
-app.post('/hello', hellobot)
+app.post('/hello', hellobot);
+app.post('/roll', dicebot);
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
